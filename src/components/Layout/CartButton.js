e9add9c858d3742e3d4e899b0429913d0b49d033
BobMakhlin <borismakhlin5@gmail.com>
2023-01-20 19:06:57 +0200

    Add animation to cart button

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,13 +1,34 @@
 import classes from "./CartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import CartContext from "../../store/cart-context";
+
+const ANIMATION_DURATION = 300;
 
 const CartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  useEffect(() => {
+    if (cartCtx.totalAmount === 0) {
+      return;
+    }
+
+    setIsAnimated(true);
+    const timerId = setTimeout(() => {
+      setIsAnimated(false);
+    }, ANIMATION_DURATION);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [cartCtx.totalAmount]);
 
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button
+      onClick={props.onClick}
+      className={`${classes.button} ${isAnimated ? classes.bump : ""}`}
+    >
       <span className={classes.icon}>
         <CartIcon />
       </span>
